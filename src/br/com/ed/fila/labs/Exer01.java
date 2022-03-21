9537191bf5be656b81053c5beda587d5ed47bdb0
heitormariano <heitor.eumesmo@gmail.com>
2022-03-21 11:32:50 -0300

    resolucao dos exercicios 01, 02 e 03 da lista

---
+++ src/br/com/ed/fila/labs/Exer01.java
@@ -0,0 +1,29 @@
+package br.com.ed.fila.labs;
+
+import br.com.ed.fila.Fila;
+
+public class Exer01 {
+
+	public static void main(String[] args) {
+		Fila<Documento> filaImpressao = new Fila<Documento>();
+
+		filaImpressao.enfileirar(new Documento("Doc A", 10));
+		filaImpressao.enfileirar(new Documento("Doc B", 15));
+		filaImpressao.enfileirar(new Documento("Doc C", 36));
+		filaImpressao.enfileirar(new Documento("Doc D", 12));
+		filaImpressao.enfileirar(new Documento("Doc E", 145));
+
+		while (!filaImpressao.estaVazia()) {
+			Documento docImprimir = filaImpressao.desenfileirar();
+			System.out.println("Imprimindo " + docImprimir);
+
+			try {
+				Thread.sleep(200 * docImprimir.getQtdPaginas());
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+		System.out.println("Os documentos da fila foram impressos");
+	}
+}
---
+++ src/br/com/ed/fila/labs/Exer02.java
@@ -0,0 +1,59 @@
+package br.com.ed.fila.labs;
+
+import br.com.ed.fila.Fila;
+
+public class Exer02 {
+
+	public static void main(String[] args) {
+		Fila<String> filaRegular = new Fila<String>();
+		Fila<String> filaPrioridade = new Fila<String>();
+		final int MAX_ATEND_PRIORIDADE = 3;
+
+		filaRegular.enfileirar("Regular 01");
+		filaRegular.enfileirar("Regular 02");
+		filaRegular.enfileirar("Regular 03");
+
+		filaPrioridade.enfileirar("Prioridade 01");
+		filaPrioridade.enfileirar("Prioridade 02");
+		filaPrioridade.enfileirar("Prioridade 03");
+
+		filaRegular.enfileirar("Regular 04");
+		filaRegular.enfileirar("Regular 05");
+
+		filaPrioridade.enfileirar("Prioridade 04");
+		filaPrioridade.enfileirar("Prioridade 05");
+
+		filaRegular.enfileirar("Regular 06");
+		filaRegular.enfileirar("Regular 07");
+		filaRegular.enfileirar("Regular 08");
+
+		while (!filaRegular.estaVazia() || !filaPrioridade.estaVazia()) {
+			int count = 0;
+
+			// atendimento prioritario (maximo de 3 pessoas)
+			while (!filaPrioridade.estaVazia() && count < MAX_ATEND_PRIORIDADE) {
+				atenderPessoa(filaPrioridade);
+				count++;
+			}
+
+			// atendimento regula (1 pessoa por vez)
+			if (!filaRegular.estaVazia()) {
+				atenderPessoa(filaRegular);
+			}
+
+			// verificar se a fila de prioridade esta vazia
+			// Se estiver, todos os atendimentos regulares sao realizados
+			if (filaPrioridade.estaVazia()) {
+				while (!filaRegular.estaVazia()) {
+					atenderPessoa(filaRegular);
+				}
+			}
+		}
+
+	}
+
+	private static void atenderPessoa(Fila<String> filaAtendimento) {
+		String pessoaAtendida = filaAtendimento.desenfileirar();
+		System.out.println("Atendimento realizado para: " + pessoaAtendida);
+	}
+}
