91b860108fb27f9fd3b7624ccfe5389530dbf219
heitormariano <heitormariano.camara@gmail.com>
2023-03-08 19:30:22 -0300

    incrementando a classe de testes

--- src/br/com/ed/parte4/lista/teste/ListaEncadeadaTeste.java
+++ src/br/com/ed/parte4/lista/teste/ListaEncadeadaTeste.java
@@ -7,7 +7,26 @@ public class ListaEncadeadaTeste {
 	public static void main(String[] args) {
 		ListaEncadeada<Integer> listaNumeros = new ListaEncadeada<>();
 		listaNumeros.adicionar(1);
+		listaNumeros.adicionar(2);
 
+		System.out.println("Tamanho = " + listaNumeros.getTamanho());
 		System.out.println(listaNumeros);
+
+		listaNumeros.adicionar(3);
+		listaNumeros.adicionar(4);
+		listaNumeros.adicionar(5);
+
+		System.out.println("Tamanho = " + listaNumeros.getTamanho());
+		System.out.println(listaNumeros);
+
+		System.out.println("Imprimindo apenas os valores da lista:");
+		System.out.println(listaNumeros.imprimirValoresLoopWhile());
+		System.out.println(listaNumeros.imprimirValoresLoopFor());
+
+		System.out.println("Lista após limpeza:");
+		listaNumeros.limpar();
+
+		System.out.println("Tamanho = " + listaNumeros.getTamanho());
+		System.out.println(listaNumeros.imprimirValoresLoopWhile());
 	}
 }
