8ffd9f655e1e2f55dd48e340aa9a59d2201411a9
heitormariano <heitormariano.camara@gmail.com>
2023-03-08 19:29:58 -0300

    lista encadeada - finalizando primeira versao de codigo

--- src/br/com/ed/parte4/lista/ListaEncadeada.java
+++ src/br/com/ed/parte4/lista/ListaEncadeada.java
@@ -3,10 +3,69 @@ package br.com.ed.parte4.lista;
 public class ListaEncadeada<T> {
 
 	private No<T> inicio;
+	private No<T> ultimo;
+	private int tamanho = 0;
 
 	public void adicionar(T elemento) {
 		No<T> celula = new No<>(elemento);
-		this.inicio = celula;
+
+		if (this.getTamanho() == 0) {
+			this.inicio = celula;
+		} else {
+			this.ultimo.setProximo(celula);
+		}
+		this.ultimo = celula;
+		this.tamanho++;
+	}
+
+	public int getTamanho() {
+		return this.tamanho;
+	}
+
+	public void limpar() {
+		for (No<T> atual = this.inicio; atual != null;) {
+			No<T> proximo = atual.getProximo();
+			atual.setElemento(null);
+			atual.setProximo(null);
+			atual = proximo;
+		}
+
+		this.inicio = null;
+		this.ultimo = null;
+		this.tamanho = 0;
+	}
+
+	// percorrendo elementos da lista (usando while)
+	public String imprimirValoresLoopWhile() {
+		if (this.tamanho == 0) {
+			return "[]";
+		}
+
+		StringBuilder builder = new StringBuilder();
+		No<T> atual = this.inicio;
+		builder.append(atual.getElemento()).append(", ");
+
+		while (atual.getProximo() != null) {
+			atual = atual.getProximo();
+			builder.append(atual.getElemento()).append(", ");
+		}
+
+		return builder.toString();
+	}
+
+	// percorrendo elementos da lista (usando for)
+	public String imprimirValoresLoopFor() {
+		StringBuilder builder = new StringBuilder();
+		No<T> atual = this.inicio;
+
+		builder.append("[");
+		for (int i = 0; i < this.tamanho - 1; i++) {
+			builder.append(atual.getElemento()).append(", ");
+			atual = atual.getProximo();
+		}
+
+		builder.append(atual.getElemento()).append("]");
+		return builder.toString();
 	}
 
 	@Override
